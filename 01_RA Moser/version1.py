b6aa49e838c4b64dffe79e0ca792c2da3052e7ff
gerald-lindner <gerald.lindner@stud.sbg.ac.at>
2016-07-13 21:50:02 +0200

    1.0 Runs with errors
    important: C:\Program
    Files\Python35\Lib\site-packages\scrapy-1.1.0-py3.5.egg\scrapy\settings
    Cookies fault, and download time to 0.26 or higher
    -still some errors in certain pages (remove spaces)
    -see if first regrex is needed

--- 01_RA Moser/version1.py
+++ 01_RA Moser/version1.py
@@ -11,126 +11,135 @@ Created on 13. Juli 2016
 #in default_settings.py
 import scrapy #the crawler import
 import re #for finding patterns
-import os
-import sys
-import base64
-import csv,cStringIO,codecs
-import twisted
-
+import os #for paths
+#import sys
+#import base64
+#import csv
+#import twisted
 from scrapy.crawler import CrawlerProcess #neccassary? test at time
-#geld=0
-#name=0
-#number=0
-#date=0
-class moseritem(scrapy.Item):
-    geld = scrapy.Field()
-    name=scrapy.Field()
-    number=scrapy.Field()
-    date=scrapy.Field()
-path3 = os.path.join(os.path.expanduser('~'), 'Documents', 'output.csv')
-path4 = os.path.join(os.path.expanduser('~'), 'Documents', 'links.csv')
-import csv #load csv into x
-with(open(path4)) as csvfile:
-    reader=csv.reader(csvfile,delimiter=';')
-    for row in reader:
-        x=row
-geld=[]
-name=[]
-number=[]
-date=[]
-address=[]
-town_to=[]#town together
-town=[]#town alone
-zip=[]
-country=[]
+import pandas as pd
+
+#paths
+path1 = os.path.join(os.path.expanduser('~'), 'Documents', 'links.txt')
+path2 = os.path.join(os.path.expanduser('~'), 'Documents', 'names.txt')
+path3 = os.path.join(os.path.expanduser('~'), 'Documents', 'names.xlsx')
+path4 = os.path.join(os.path.expanduser('~'), 'Documents', 'links.xlsx')
+
+#read files
+txt = open(path1,'r')
+text1=txt.read()
+txt2 = open(path2,'r')
+text2=txt2.read()
+
+#create patterns
+pattern1= "Rechung =C3=B6ffnen =\n<(.*\n.*)"
+pattern2='Subject.*\n(.*)\n\n(.*)\n(.*)\n(.*)'
+
+#create dataframe
+columns = ['Name','Adresse', 'PLZ','Staat']
+data_names=pd.DataFrame(columns=columns)
+
+
+#m1,pattern1=hyperlinks, m2,pattern2=names
+m1 = re.findall(pattern1,text1)
+m1 = [w.replace('\n', '') for w in m1]
+m1 = [w.replace('3D', '') for w in m1]
+m1 = [w.replace('>=20', '') for w in m1]
+m1= [w.replace('re_e=uro=1','re_euro=1')for w in m1]
+
+m2=re.findall(pattern2,text2)
+
+#create dataframe for names, double check with data_money
+for x in range (0,len(m2)):
+    data_names.loc[x]=[m2[x][0],m2[x][1],m2[x][2],m2[x][3]]
+    x+=1
+#must be a better way to do this
+op1=[]
+op2=[]
+op3=[]
+op4=[]
+op5=[]
+op6=[]
+op7=[]
+op8=[]
+op9=[]
 class ramoser(scrapy.Spider): #scrapy class, contains everything
     name = 'ramoser'
-    start_urls = x
+    start_urls = m1[0:10] #for experiment reasosn
     def parse(self,response):
-        try: 
-            geld_v=response.xpath('//strong/text()')[15].extract()
-            geld_v=geld_v.replace('\r', '')
-            geld_v=geld_v.replace("'", '')
-            geld_v=geld_v.replace('\n', '')
-            geld.append(geld_v)
-        except: geld.append("NA")
-        try:
+        try: #here i still have the problem of not getting spaces out
             name_v=response.xpath('//strong/text()')[4].extract()
             name_v=name_v.replace('\r', '')
             name_v=name_v.replace('"', '')
             name_v=name_v.replace('\n', '')
             name_v=name_v.replace('*', '')
-            name_v=name_v.replace('            ','')
-            name.append(name_v)
-        except: name.append("NA")
+            name_v=name_v.strip()
+            op1.append(name_v)
+        except: 
+            op1.append("NA")
+        
         try:
             address_v=response.xpath('//strong/text()')[5].extract()
-            address.append(address_v)
-        except: address.append("NA")
+            op2.append(address_v)
+        except: 
+            op2.append("NA")
+        
         try:
             town_to=response.xpath('//strong/text()')[6].extract()
-            town_to=town_to.split(" ",1)#only one time
-            town.append(town_to[1])
-            zip.append(town_to[0])
+            town_to2=town_to.split(" ",1)#only one time
+            op3.append(town_to2[0])
+            op4.append(town_to2[1])
+            op5.append(town_to)
         except:
-            zip.append("NA")
-            town.append("NA")
-        try:
-            number_v=response.xpath('//strong/text()')[8].extract()
-            number_v=number_v.split(":")
-            number.append(number_v[1])
-        except: number.append("NA")
+            op3.append("NA")
+            op4.append("NA")
+            op5.append("NA")
+            
         try:
             country_v=response.xpath('//strong/text()')[7].extract()
             country_v=country_v.replace('\r', '')
             country_v=country_v.replace('"', '')
             country_v=country_v.replace('\n', '')
             country_v=country_v.replace('*', '')
-            country.append(country_v)
-        except:country.append("NA")
+            op6.append(country_v)
+        except:
+            op6.append("NA")
+        
+        try:
+            number_v=response.xpath('//strong/text()')[8].extract()
+            number_v=number_v.split(":")
+            op7.append(number_v[1])
+        except: 
+            op7.append("NA")
+            
+        try: 
+            geld_v=response.xpath('//strong/text()')[15].extract()
+            geld_v=geld_v.replace('\r', '')
+            geld_v=geld_v.replace("'", '')
+            geld_v=geld_v.replace('\n', '')
+            op8.append(geld_v)
+        except: 
+            op8.append("NA")
+            
         try:
             date_v=response.xpath('//span/text()')[4].extract()
-            date.append(date_v)
+            op9.append(date_v)
         except:
-            date.append("NA")
-            
+            op9.append("NA")
+
+
+         
 process = CrawlerProcess()
 process.crawl(ramoser)
 process.start()
+#dataframe for crawling results
+columns2 = ['Name','Adresse', 'PLZ','Ort','Adresse und PLZ','Staat','Nummer',"Geld",'Datum']
+data_money=pd.DataFrame(columns=columns2)
+for i in range(0,len(op1)):
+    all=data_money.loc[i]=[op1[i],op2[i],op3[i],op4[i],op5[i],op6[i],op7[i],op8[i],op9[i]]
+    data_money.append(all)
 
-class UnicodeWriter:#simply to make utf-8 work
-    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
-        # Redirect output to a queue
-        self.queue = cStringIO.StringIO()
-        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
-        self.stream = f
-        self.encoder = codecs.getincrementalencoder(encoding)()
-
-    def writerow(self, row):
-        self.writer.writerow([s.encode("utf-8") for s in row])
-        # Fetch UTF-8 output from the queue ...
-        data = self.queue.getvalue()
-        data = data.decode("utf-8")
-        # ... and reencode it into the target encoding
-        data = self.encoder.encode(data)
-        # write to the target stream
-        self.stream.write(data)
-        # empty queue
-        self.queue.truncate(0)
-
-    def writerows(self, rows):
-        for row in rows:
-            self.writerow(row)
-
-
-with open(path3,'wb') as fout:
-    writer = UnicodeWriter(fout)
-    writer.writerow(name)
-    writer.writerow(address)
-    writer.writerow(zip)
-    writer.writerow(town)
-    writer.writerow(country)
-    writer.writerow(number)
-    writer.writerow(geld)
-    writer.writerow(date)
+writer = pd.ExcelWriter(path3)
+data_money.to_excel(writer,'Sheet1')
+writer.save()
 
